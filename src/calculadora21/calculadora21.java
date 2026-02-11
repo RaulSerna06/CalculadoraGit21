431d16d725fbddcc7b2bc14ee351af19be05c400
main <raul.serna06@somo.eus>
2026-02-11 22:37:56 +0100

    Cambio en dividir: manejo de error sin excepción

--- src/calculadora21/calculadora21.java
+++ src/calculadora21/calculadora21.java
@@ -12,7 +12,20 @@ public class calculadora21 {
  public int multiplicar(int a, int b) { return a * b;
  }
 
- public double dividir(int a, int b) { return (double) a / b;
+ public double dividir(int a, int b) { if (b == 0) {
+	 System.out.println("Error: división entre cero"); return 0;
+	 }
+	 return (double) a / b;
+	 }
+
+ public int potencia(int base, int exponente) { int resultado = 1;
+ for (int i = 0; i < exponente; i++) { resultado *= base;
  }
+ return resultado;
+ }
+
+ public int modulo(int a, int b) { return a % b;
+ }
+
 
 }
