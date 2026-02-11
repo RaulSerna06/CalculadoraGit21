5a8e855c6d97d0f28c92e8678bea2d9ccf54cf4b
main <raul.serna06@somo.eus>
2026-02-11 20:09:48 +0100

    Añadiendo metodo de potencias y modulo

--- src/calculadora21/calculadora21.java
+++ src/calculadora21/calculadora21.java
@@ -12,7 +12,18 @@ public class calculadora21 {
  public int multiplicar(int a, int b) { return a * b;
  }
 
- public double dividir(int a, int b) { return (double) a / b;
+ public double dividir(int a, int b) { if (b == 0) {
+	 throw new IllegalArgumentException("No se puede dividir entre cero");
+	 }
+	 return (double) a / b;
+}
+ public int potencia(int base, int exponente) { int resultado = 1;
+ for (int i = 0; i < exponente; i++) { resultado *= base;
+ }
+ return resultado;
+ }
+
+ public int modulo(int a, int b) { return a % b;
  }
 
 }
